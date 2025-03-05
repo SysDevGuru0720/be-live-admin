eddd61248e19f076ac3f57cf44b0230bb2efe330
SysDevGuru0720 <leahucosmin0720@gmail.com>
2025-03-05 15:29:29 -0500

    Delete frontend(it is another project)

--- be-live-api/dto/category.go
+++
@@ -1,6 +0,0 @@
-package dto
-
-type CategoryDto struct {
-	ID   uint   `json:"id"`
-	Name string `json:"name"`
-}
--- be-live-api/dto/mail.go
+++
@@ -1,7 +0,0 @@
-package dto
-
-type MailModel struct {
-	Email   string `json:"email"`
-	Subject string `json:"subject"`
-	Content string `json:"content"`
-}
